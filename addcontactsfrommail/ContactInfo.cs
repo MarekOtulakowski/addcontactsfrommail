cd15c848e35be3483a2c3860eaf148387bd430c7
marek.otulakowski@gmail.com <marek.otulakowski@gmail.com@0e936cc9-e743-9af6-251a-6def1bc7b4bc>
2011-12-04 23:07:49 +0000

    v.1.1 beta -not fully implement and not tested application in this version

---
+++ addcontactsfrommail/ContactInfo.cs
@@ -0,0 +1,25 @@
+﻿#region UsingDirectives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#endregion
+
+namespace addcontactsfrommail
+{
+    /// <summary>
+    /// Class object for stored contact info
+    /// </summary>
+    public class ContactInfo
+    {
+        /// <summary>
+        /// sender name value
+        /// </summary>
+        public string SenderName { get; set; }
+
+        /// <summary>
+        /// sender mail adress value
+        /// </summary>
+        public string SenderMail { get; set; }
+    }
+}
